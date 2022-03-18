021519b2b01a6f545c036b3db59478fd34d7f6b4
Frija-Altarac <liam.frija-altarac.1@ens.etsmtl.ca>
2022-03-18 15:45:43 -0400

    added to new method

--- cnn_feature_extractor.py
+++ cnn_feature_extractor.py
@@ -36,7 +36,7 @@ class CNNFeatureExtractor:
                     input_shape=(224, 224, 3))
         
 
-    def load_img(self, img_path, preprocess=True):
+    def load_img(self, img_path, img_shape, preprocess=True):
         img_shape=[224,224]
         img_rows=img_shape[0]
         img_cols=img_shape[1]
@@ -59,102 +59,71 @@ class CNNFeatureExtractor:
         #print('labels shape',labels.shape)
         return np.array(img)
 
-    def _cnn_lap(self, image, layer_name):
-        min_max_scaler = preprocessing.MinMaxScaler()
-        layer = [l for l in self.model.layers if layer_name == l.name][0]
-        #print(layers)
-        mag = []
-        if type(layer.output_shape) is list:
-            num_channels = layer.output_shape[0][-1]
-        else:
-            num_channels = layer.output_shape[-1]
-        #print(num_channels)
-
-        imgs = []
-        get_layer_output = K.function([self.model.input], layer.output)
-        layer_out = np.array(get_layer_output(image))
-        for i in range(0, num_channels):
-            #plt.imshow(layer_out[0][:,:,2])
-            imgs.append(layer_out[0][:,:,i])
-
+    def lap_mag(self , channels):
         laps = []
-        for i in range(0, num_channels):
-            out = imgs[i]
-            dst = cv2.Laplacian(out, cv2.CV_32F , 7)
+        #print(channels.shape)
+        for i in range(channels.shape[-1]):
+            
+            dst = cv2.Laplacian(channels[:,:,i], cv2.CV_32F , 3)
             laps.append(dst)
+        mag = np.linalg.norm(laps, axis = 0)
+        return mag
 
-        mag.append(np.linalg.norm(np.abs(laps), axis = 0))
-        #mag.append(np.sum(laps, axis = 0))
-        #scaled_mag = min_max_scaler.fit_transform(mag[0])
+    def get_cnn_out(self, input, layer_num):
+        get_layer_output = K.function([self.model.input], [l.output for l in self.model.layers][layer_num])
+        layer_out = np.array(get_layer_output(input))
+        return layer_out[0]
 
-        #mag /= mag.max()/255.0' ''
-        return mag[0] * 1.0/mag[0].max()  , imgs, np.array(laps)
-        #return dst
 
-    def _local_arg_exrtrema_2(self, mat1, mat2):
 
+
+    def local_arg_max(self, mat, window_size):
         #Use a moving window to find local max/min in section. Determine coordinate of max pixel in image.
         idx = []
-        thresh = np.abs(np.amax(mat2) - np.abs(np.amin(mat2)))
-        #print("Thresh = ", thresh)
 
-        r = cv2.cornerHarris(mat1,5,5,0.04, cv2.BORDER_ISOLATED)
-        r = cv2.dilate(r,None)
-        for i in range(1, mat1.shape[0]-1):
-            for j in range(1, mat1.shape[1]-1):
-                #if r[i,j]  > 0.01*np.amax(r) : #if pixel is not on edge
-                pixel_of_interest = mat2[i,j]
-                #if pixel_of_interest>0.15:
+        k = int(np.floor(window_size/2))
+        #print(k)
+        for i in range(k, mat.shape[0]-k, window_size):
+            for j in range(k, mat.shape[1]-k, window_size):
 
-                neighbours = mat2[i-1:i+2, j-1:j+2]
-                neighbours[1,1] = np.NaN
-                neighbours_below = mat1[i-1:i+2, j-1:j+2]
-                if (pixel_of_interest > np.nanmax(neighbours) and pixel_of_interest > np.nanmax(neighbours_below)) or (pixel_of_interest < np.nanmin(neighbours) and pixel_of_interest < np.nanmin(neighbours_below)):
-                    idx.append(np.array([j,i]))
+                window = mat[i-k:i+k+1, j-k:j+k+1]
+                coords = np.argwhere(window==window.max())
+                
+                idx.extend(coords + [i-k, j-k])
 
-        return np.unique(idx, axis=0)
+        return  np.unique(idx, axis=0)  
 
-    def _getDescriptors(self, layer_output, coords):
-        #fig2 = plt.figure(figsize=(30, 30))
+    def get_keypoints_and_descriptors(self, image_path):
 
-        layers = []
-        '''for i in range(len(layer_output)):
-            layer_output[i] = cv2.resize(layer_output[i], (224, 224), interpolation = cv2.INTER_LINEAR )'''
-        layer_output = np.stack(layer_output)
-        '''for i in range(0,64):
-            layers.append(layer_output[:,:,i])'''
-        #layers = np.stack(layers)
+        img = self.load_img(image_path, [224,224])
+        img_no_proc = self.load_img(image_path, [224,224], preprocess=False)
+        #keypoint_coords = np.array([])
 
-        descriptors = []
-        for col, row in  coords:
-            #d_vec = np.array(layer_output[:,row, col ]).flatten()
-            d_vec = np.array(layer_output[:,row-1:row+2, col-1:col+2 ]).flatten()
-            d_vec = np.abs(d_vec)
-            d_vec *= 1.0/d_vec.max()
-            descriptors.append(d_vec)
-        return descriptors
 
-    def get_keypoints_and_descriptors(self, img):
-        keypoint_coords = []
 
-        mag_1,_,_ = self._cnn_lap(img, layer_name=self.model.layers[0].name)
-        mag_2 ,layer_out,lap_out_2 = self._cnn_lap(img, layer_name=self.model.layers[1].name)
-        mag_3,out, lap_out_3 = self._cnn_lap(img, layer_name=self.model.layers[2].name)
-        #_,layer_out,_ = cnn_lap(img, layer_name=model.layers[7].name)
-        
-        keypoint_coords = np.unique(self._local_arg_exrtrema_2(mag_1, mag_2), axis=0)
+        layer_1 = self.get_cnn_out(img, 1)  
+        layer_2 =  self.get_cnn_out(img, 2)
+        block_1 = (self.lap_mag(layer_1) +  self.lap_mag(layer_2)) /2
+        layer_output = np.stack([layer_1, layer_2])
 
-        desc = self._getDescriptors(np.concatenate([lap_out_2, lap_out_3]), keypoint_coords)
+        keypoint_coords = self.local_arg_max(block_1, 3)
+        del layer_1, layer_2, block_1
 
         k=[]
+        d=[]
+        gray = cv2.cvtColor(img_no_proc,cv2.COLOR_BGR2GRAY)
+        r = cv2.cornerHarris(gray,5,5,0.04, cv2.BORDER_ISOLATED)
+        r = cv2.dilate(r,None)
         for row, col in keypoint_coords:
             #print(float(row), float(col))
-            keypoint = cv2.KeyPoint()
-            keypoint.pt = (float(row), float(col))
-            keypoint.octave = 0
-            keypoint.size = 0
-            keypoint.response = 0
-            k.append(keypoint)
+            if r[row,col]  > 0.01*np.amax(r) : #if pixel is not on edge
 
+                keypoint = cv2.KeyPoint()
+                keypoint.pt = (float(col), float(row))
+                keypoint.octave = 0
+                keypoint.size = 0
+                keypoint.response = 0
+                k.append(keypoint)
+                d.append(np.array(layer_output[:, row, col ]).flatten())
 
-        return np.array(k), np.array(desc)
+        return np.array(k), np.array(d)
