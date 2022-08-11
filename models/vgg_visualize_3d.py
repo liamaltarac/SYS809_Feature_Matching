24824db3b48ad214effda1be2d9302808efd9c95
Frija-Altarac <liam.frija-altarac.1@ens.etsmtl.ca>
2022-08-11 14:55:32 -0400

    added functionality to click on point to visualize individual filter

--- models/vgg_visualize_3d.py
+++ models/vgg_visualize_3d.py
@@ -33,7 +33,7 @@ from tensorflow.keras.applications import VGG16
 from mayavi  import mlab 
 
 
-
+RGB = ['R','G','B']
 
 model = VGG16(weights='imagenet',
                   include_top=False,
@@ -84,36 +84,51 @@ def NormalizeData(data):
     return (data - np.min(data)) / (np.max(data) - np.min(data))
 
 
-'''
-Display Functions
-'''
-def onpick(event):
-    thisline = event.artist
-    #xdata = thisline.get_xdata()
-    #ydata = thisline.get_ydata()
-    #ind = event.ind
-    #points = tuple(zip(xdata[ind], ydata[ind]))
-    print('onpick points:', thisline.get_gid())
+
+filters = get_filter(1)
+
+fig = mlab.figure(1)
+mlab.clf()
+
+#https://stackoverflow.com/questions/24308049/how-to-plot-proper-3d-axes-in-mayavi-like-those-found-in-matplotlib
+lensoffset = 0
+xx = yy = zz = np.arange(-1.5,1.5,0.1)
+xy = xz = yx = yz = zx = zy = np.zeros_like(xx)    
+mlab.plot3d(yx,yy+lensoffset,yz,line_width=0.01,tube_radius=0.01)
+mlab.plot3d(zx,zy+lensoffset,zz,line_width=0.01,tube_radius=0.01)
+mlab.plot3d(xx,xy+lensoffset,xz,line_width=0.01,tube_radius=0.01)
+
+cursor3d = mlab.points3d(0., 0., 0., mode='axes',
+                                color=(0, 0, 0),
+                                scale_factor=0.5)
 
 
+filter_list = dict()
+sym_list = dict()
+anti_list = dict()
 
-if __name__ == "__main__":
+zdata = np.array([])
+xdata = np.array([])
+ydata = np.array([])
 
-    filters = get_filter(1)
+glyphs = []
 
-    fig = plt.figure()
+num_filters = filters.shape[-1]
+num_channels = filters[:,:,:, 0].shape[-1]
+for i in range(num_channels):  # Each channel (ex R G B)
 
-    ax = plt.axes(projection='3d')
+    z = np.array([])
+    x = np.array([])
+    y = np.array([])
 
-    for i in range(filters.shape[-1]):
-        filter = filters[:,:,:, i]
-        #for j in range(filter.shape[-1]):
-        f = filter[:,:, 1]
-        # Data for a three-dimensional line
-        '''zline = np.linspace(0, 15, 1000)
-        xline = np.sin(zline)
-        yline = np.cos(zline)
-        ax.plot3D(xline, yline, zline, 'gray')'''
+    filter_list[i] = []
+    sym_list[i] = []
+    anti_list[i] = []
+
+
+    for j in range(num_filters): #each filter in that channel
+        f = filters[:,:,:, j]
+        f = f[:,:, i]  
 
         sym, anti = getSymAntiSym(normalizeToOne(f))
         sym_mag = np.linalg.norm(sym)
@@ -123,18 +138,71 @@ if __name__ == "__main__":
         theta = theta[theta.shape[0]//2, theta.shape[1]//2]
 
         # Data for three-dimensional scattered points
-        zdata = sym_mag
-        xdata = anti_mag*np.sin(theta)
-        ydata = anti_mag*np.cos(theta) 
+        z = np.append(z, sym_mag)
+        y = np.append(y, anti_mag*np.sin(theta))
+        x = np.append(x, anti_mag*np.cos(theta) )
 
-        print(zdata.shape, xdata.shape, ydata.shape)
-        mlab.points3d(xdata, ydata, zdata,  colormap='Spectral', scale_factor=.1)    
+        filter_list[i].append(f)
+        sym_list[i].append(sym)
+        anti_list[i].append(anti)
+    glyphs.append(mlab.points3d(x, y, z,  color = tuple(np.eye(num_channels)[i].astype(int)), scale_factor=.1))
+    zdata = np.append(zdata, z)
+    ydata = np.append(ydata, y)
+    xdata = np.append(xdata, x)
+
+    glyph_points = glyphs[-1].glyph.glyph_source.glyph_source.output.points.to_array()
+
+print(xdata.shape,ydata.shape,zdata.shape )
+
+#print(zdata.shape, xdata.shape, ydata.shape)
+# Every object has been created, we can reenable the rendering.
+#fig.scene.disable_render = False
+
+# Here, we grab the points describing the individual glyph, to figure
+# out how many points are in an individual glyph.
+
+
+#points_dict[r] = (i, f, sym, anti)
+#print(r.actor.actor._vtk_obj)
+
+
+
+'''
+Display Functions
+'''
+def picker_callback(picker_obj):
+    fig, ax = plt.subplots(ncols=3)
+
+    for n, g in enumerate(glyphs):
+        if picker_obj.actor in g.actor.actors :
+
+            # m.mlab_source.points is the points array underlying the vtk
+            # dataset. GetPointId return the index in this array.
+            point_id = picker_obj.point_id//glyph_points.shape[0]
+                                                                
+            # If the no points have been selected, we have '-1'
+            if point_id != -1:
+                # Retrieve the coordinates coorresponding to that data
+                # point
+                x, y, z = xdata[point_id], ydata[point_id], zdata[point_id]
+                #print(x,y,z, type(point_id), point_id, filter_list.shape)
+
+                f = filter_list[n][point_id]
+                sym = sym_list[n][point_id]
+                anti_sym = anti_list[n][point_id]
+
+                ax[0].imshow(f, cmap=plt.get_cmap('gray')) 
+                ax[0].set_title('Filter : {} , Chanel : {}'.format(point_id, RGB[n]))
     
-    #https://stackoverflow.com/questions/24308049/how-to-plot-proper-3d-axes-in-mayavi-like-those-found-in-matplotlib
-    lensoffset = 0
-    xx = yy = zz = np.arange(-1.5,1.5,0.1)
-    xy = xz = yx = yz = zx = zy = np.zeros_like(xx)    
-    mlab.plot3d(yx,yy+lensoffset,yz,line_width=0.01,tube_radius=0.01)
-    mlab.plot3d(zx,zy+lensoffset,zz,line_width=0.01,tube_radius=0.01)
-    mlab.plot3d(xx,xy+lensoffset,xz,line_width=0.01,tube_radius=0.01)
-    mlab.show()
+                ax[1].imshow(sym, cmap=plt.get_cmap('gray')) 
+                ax[2].imshow(anti_sym, cmap=plt.get_cmap('gray')) 
+                fig.show()
+
+
+picker = fig.on_mouse_pick(picker_callback)
+
+# Decrease the tolerance, so that we can more easily select a precise
+# point.
+picker.tolerance = 0.01
+
+mlab.show()
